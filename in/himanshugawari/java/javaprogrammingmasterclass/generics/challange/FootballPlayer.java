268df930c276b6ab2eba417a8d7828b42609c082
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-14 23:37:04 +0530

    football player

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/generics/challange/FootballPlayer.java
@@ -0,0 +1,7 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.generics.challange;
+
+public class FootballPlayer extends Player {
+  public FootballPlayer(String name) {
+    super(name);
+  }
+}
