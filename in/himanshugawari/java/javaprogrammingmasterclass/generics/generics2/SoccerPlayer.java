b7010fc6e1672ede35ee3b1b1c996b0852d276a9
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-14 15:21:29 +0530

    soccer player extends player

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/generics/generics2/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.generics.generics2;
+
+public class SoccerPlayer extends Player {
+  public SoccerPlayer(String name) {
+    super(name);
+  }
+}
