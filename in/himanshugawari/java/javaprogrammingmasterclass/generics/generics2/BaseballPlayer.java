a04fa9658b58a274bdc2c329dc7af0d4ef831c12
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-14 15:21:07 +0530

    baseball player extends player

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/generics/generics2/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.generics.generics2;
+
+public class BaseballPlayer extends Player {
+  public BaseballPlayer(String name) {
+    super(name);
+  }
+}
