ebfa7443351c9fdc78f6a8ccabfcb3027e8448b3
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-14 22:59:25 +0530

    update

--- in/himanshugawari/java/javaprogrammingmasterclass/generics/generics2/Main.java
+++ in/himanshugawari/java/javaprogrammingmasterclass/generics/generics2/Main.java
@@ -6,11 +6,39 @@ public class Main {
     BaseballPlayer pat = new BaseballPlayer("Pat");
     SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-    Team himanshu = new Team("Himanshu");
-    himanshu.addPlayer(joe);
-    himanshu.addPlayer(pat);
-    himanshu.addPlayer(beckham);
+    Team<FootballPlayer> footballTeam = new Team<>("FootballTeam");
+    footballTeam.addPlayer(joe);
 
-    System.out.println(himanshu.numPlayers());
+    Team<BaseballPlayer> baseballTeam = new Team<>("BaseballTeam");
+    baseballTeam.addPlayer(pat);
+
+    Team<SoccerPlayer> soccerTeam = new Team<>("SoccerTeam");
+    soccerTeam.addPlayer(beckham);
+
+    Team<FootballPlayer> footballTeam1 = new Team<>("FootballTeam1");
+    Team<FootballPlayer> footballTeam2 = new Team<>("FootballTeam2");
+
+    footballTeam1.matchResult(footballTeam2, 1, 0);
+    footballTeam1.matchResult(footballTeam, 3, 8);
+    footballTeam.matchResult(footballTeam2, 2, 1);
+
+    String p = "Players in ";
+    System.out.println(p + footballTeam.getName() + " is " + footballTeam.numPlayers());
+    System.out.println(p + footballTeam1.getName() + " is " + footballTeam1.numPlayers());
+    System.out.println(p + footballTeam2.getName() + " is " + footballTeam2.numPlayers());
+    System.out.println(p + baseballTeam.getName() + " is " + baseballTeam.numPlayers());
+    System.out.println(p + soccerTeam.getName() + " is " + soccerTeam.numPlayers());
+
+    System.out.println("Rankings");
+    System.out.println(footballTeam.getName() + " : " + footballTeam.ranking());
+    System.out.println(footballTeam1.getName() + " : " + footballTeam1.ranking());
+    System.out.println(footballTeam2.getName() + " : " + footballTeam2.ranking());
+    System.out.println(baseballTeam.getName() + " : " + baseballTeam.ranking());
+    System.out.println(soccerTeam.getName() + " : " + soccerTeam.ranking());
+
+    System.out.println(footballTeam.compareTo(footballTeam1));
+    System.out.println(footballTeam.compareTo(footballTeam2));
+    System.out.println(footballTeam2.compareTo(footballTeam));
+    System.out.println(footballTeam.compareTo(footballTeam));
   }
 }
