d6dfc844749f3bd6dfcccf269d2f2ce9be7377e0
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-14 23:36:50 +0530

    baseball player

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/generics/challange/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.generics.challange;
+
+public class BaseballPlayer extends Player {
+  public BaseballPlayer(String name) {
+    super(name);
+  }
+}
