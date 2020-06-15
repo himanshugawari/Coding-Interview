c585442dc4fbac89077d9198296674bbdacb6f39
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-15 14:35:26 +0530

    added player class

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/interfaces/challange/Player.java
@@ -0,0 +1,79 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.interfaces.challange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+
+	private String name;
+	private int hitPoints;
+	private int strength;
+	private String waepon;
+
+	public Player(String name, int hitPoints, int strength) {
+		super();
+		this.name = name;
+		this.hitPoints = hitPoints;
+		this.strength = strength;
+		this.waepon = "Sword";
+	}
+
+	@Override
+	public List<String> write() {
+		List<String> values = new ArrayList<String>();
+		values.add(0, this.name);
+		values.add(1, "" + this.hitPoints);
+		values.add(2, String.valueOf(this.strength));
+		values.add(3, this.waepon);
+		return values;
+	}
+
+	@Override
+	public void read(List<String> savedValues) {
+		if (savedValues != null && savedValues.size() > 0) {
+			this.name = savedValues.get(0);
+			this.hitPoints = Integer.parseInt(savedValues.get(1));
+			this.strength = Integer.parseInt(savedValues.get(2));
+			this.waepon = savedValues.get(3);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Player [name=" + name + ", hitPoints=" + hitPoints + ", strength=" + strength + ", waepon=" + waepon
+				+ "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public String getWaepon() {
+		return waepon;
+	}
+
+	public void setWaepon(String waepon) {
+		this.waepon = waepon;
+	}
+
+}
