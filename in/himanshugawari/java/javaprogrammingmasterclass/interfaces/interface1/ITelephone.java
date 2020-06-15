496ad1e4a189cc03568ab5b21c6c6ce5f817ce0d
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-15 13:13:56 +0530

    added ITelephone interface

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/interfaces/interface1/ITelephone.java
@@ -0,0 +1,13 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.interfaces.interface1;
+
+public interface ITelephone {
+	void powerOn();
+
+	void dial(int phoneNumber);
+
+	void answer();
+
+	boolean callPhone(int phoneNumber);
+
+	boolean isRinging();
+}
