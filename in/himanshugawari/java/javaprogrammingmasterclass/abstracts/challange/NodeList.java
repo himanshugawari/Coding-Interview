90c67e56881f0c2b86c1ecba923dc7d1dbfd731b
Himanshu Gawari <himanshu25031991@gmail.com>
2020-06-16 19:46:32 +0530

    NodeList interface

--- in/himanshugawari/java/javaprogrammingmasterclass/abstracts/challange/NodeList.java
+++ in/himanshugawari/java/javaprogrammingmasterclass/abstracts/challange/NodeList.java
@@ -1,11 +1,12 @@
 package in.himanshugawari.java.javaprogrammingmasterclass.abstracts.challange;
 
 public interface NodeList {
+	
 	ListItem getRoot();
 
 	boolean addItem(ListItem item);
 
 	boolean removeItem(ListItem item);
 
-	void traverse(ListItem root);
+	void traverse(ListItem item);
 }
