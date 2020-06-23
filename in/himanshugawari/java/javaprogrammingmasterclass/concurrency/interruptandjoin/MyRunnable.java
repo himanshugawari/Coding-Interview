83397801fb7264c71dc97bf7a66ec04e3e7d7ed9
himanshu <himanshu25031991@gmail.com>
2020-06-23 21:47:31 +0530

    interrupt and join, MyRunnable implements Runnable

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/concurrency/interruptandjoin/MyRunnable.java
@@ -0,0 +1,8 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.concurrency.interruptandjoin;
+
+public class MyRunnable implements Runnable {
+	@Override
+	public void run() {
+		System.out.println("In MYRunnable Thread");
+	}
+}
