fc8d77ab3116868a51ac3bc732c7dcf14ca8a80c
himanshu <himanshu25031991@gmail.com>
2020-06-23 21:48:17 +0530

    MyRunnable implements Runnable

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/concurrency/runnable1/MyRunnable.java
@@ -0,0 +1,14 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.concurrency.runnable1;
+
+/**
+ * @author himanshu
+ *
+ */
+public class MyRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Myrunnable thread using runnable interface");
+	}
+
+}
