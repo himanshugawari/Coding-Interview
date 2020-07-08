6df1cafb823fc8aa137047a469100842d6fb6203
himanshu <himanshu25031991@gmail.com>
2020-07-08 23:25:26 +0530

    composition example

---
+++ in/himanshugawari/java/javaprogrammingmasterclass/oop/composition/Case.java
@@ -0,0 +1,48 @@
+package in.himanshugawari.java.javaprogrammingmasterclass.oop.composition;
+
+/**
+ * @author himanshu
+ *
+ */
+public class Case {
+
+	private String model;
+	private String manufacturer;
+	private String powerSupply;
+	private Dimensions dimensions;
+
+	public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+		super();
+		this.model = model;
+		this.manufacturer = manufacturer;
+		this.powerSupply = powerSupply;
+		this.dimensions = dimensions;
+	}
+
+	public void pressPowerButton() {
+		System.out.println("Power button pressed");
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getPowerSupply() {
+		return powerSupply;
+	}
+
+	public Dimensions getDimensions() {
+		return dimensions;
+	}
+
+	@Override
+	public String toString() {
+		return "Case [model=" + model + ", manufacturer=" + manufacturer + ", powerSupply=" + powerSupply
+				+ ", dimensions=" + dimensions.toString() + "]";
+	}
+
+}
